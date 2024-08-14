5cb135ba5dbdc0d1a0836f7958c3b979a3482291
AlexeyZhurauliou <aliaksei.zhurauliou@yandex.by>
2024-08-14 13:42:52 +0300

    update

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,6 +6,7 @@ sudo apt update
 # Установка Ansible
 sudo apt install -y ansible
 
+sudo ansible-galaxy collection install kubernetes.core
 # Клонирование репозитория в /root (не рекомендуется, но оставлено как в исходном скрипте)
 sudo git clone https://github.com/aliakseizhyrauliou/k3s.git /root/k3s
 
